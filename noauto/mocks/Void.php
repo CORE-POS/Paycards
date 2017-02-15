5c2afbe39a73cd3edd9fb92d07f17ec63c353cb2
Andy Theuninck <andy@gohanman.com>
2017-02-14 19:15:24 -0600

    Inject session into Void parsers

--- noauto/mocks/Void.php
+++ noauto/mocks/Void.php
@@ -4,6 +4,10 @@ namespace COREPOS\pos\parser\parse;
 
 class Void
 {
+    public function __construct($session)
+    {
+    }
+
     public function voidid($id, $json)
     {
         return array();
